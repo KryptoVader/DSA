2a171be9da239eb728ec063c618243a5da84a534
KryptoVader <divyansh.shekhar2004@gmail.com>
2026-09-06 17:37:36 +0530

    Fixed size window implemented

---
+++ Algorithm/Arrays/sliding.py
@@ -0,0 +1,13 @@
+def cus(og, i,o):
+    return og + i - o;
+
+def max_sum_subarray(arr, k):
+    s = sum(arr[:k])
+    max_sum = s
+
+    for i in range(1, len(arr) - k + 1):
+        s = s - arr[i-1] + arr[i+k-1]
+        max_sum = max(max_sum, s)
+    return max_sum
+
+print(max_sum_subarray([2, 1, 5, 1, 3, 2], 3))
