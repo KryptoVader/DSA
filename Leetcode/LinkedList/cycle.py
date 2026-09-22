dea7d2c87a7e90b42c10f2a60927e9673cc17a82
KryptoVader <divyansh.shekhar2004@gmail.com>
2026-09-22 18:42:40 +0530

    Solved cycle detection in LinkedList

---
+++ Leetcode/LinkedList/cycle.py
@@ -0,0 +1,10 @@
+def hasCycle(self, head):
+        slow, fast = head, head
+        
+        while fast and fast.next:
+            fast = fast.next.next
+            slow = slow.next
+
+            if fast == slow:
+                return True
+        return False
